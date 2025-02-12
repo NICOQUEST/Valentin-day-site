a381d58ea7d555539f7ae0dccd2a0706d076c642
NICOQUEST <nicolas.mnr09@gmail.com>
2025-02-12 18:51:29 +0100

    Add files via upload
    Ajout des fichiers du site

---
+++ script.js
@@ -0,0 +1,4 @@
+function startGame() {
+    alert("Première énigme : Quel est notre endroit préféré pour une promenade ? ??");
+    // Tu peux ensuite ajouter plus d'énigmes ou rediriger vers une autre page.
+}
